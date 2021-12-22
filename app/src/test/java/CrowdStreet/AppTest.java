57dd311adca9b25576876ca9681c9c80f0c5db70
Gala <galynakm7@gmail.com>
2021-12-21 23:39:41 -0600

    create maven project

---
+++ app/src/test/java/CrowdStreet/AppTest.java
@@ -0,0 +1,21 @@
+package CrowdStreet;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CROWDSTREET {
+    
+}
